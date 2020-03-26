f68745b71e0d09fe833d01a308414c2f60f755f4
Senthilkumar <senthilkumar.b3@happiestminds.com>
2020-03-26 18:50:49 +0530

    My Portfolio action & view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  #get 'users/my_portfolio'
   devise_for :users
-    root 'welcome#index'
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+  get 'my_portfolio', to: 'users#my_portfolio'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
